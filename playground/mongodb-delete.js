ff659caf01c0777f6c6a5fe0b932490b6c4942c2
Anh Vu <anhvu869492@gmail.com>
2017-03-25 22:15:52 +0700

    Add POST /todo ad refactor mongoose

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -9,25 +9,25 @@ MongoClient.connect(url,(err, db) =>{
   }
   console.log('Connected to MongoDB server');
 
-  //deleteMany
+  // deleteMany
   // db.collection('Todos').deleteMany({text:'Eat lunch'}).then((result) =>{
   //   console.log(result);
   // });
-
-  //deleteOne
+  //
+  // deleteOne
   // db.collection('Todos').deleteOne({text:'Eat lunch'}).then((result) => {
   //   console.log(result);
   // });
-
-  //findOneAndDelete
+  //
+  // findOneAndDelete
   // db.collection('Todos').findOneAndDelete({completed: false}).then((result) =>{
   //   console.log(result);
   // });
-
+  //
   // db.collection('Users').deleteMany({name: 'Anh Vu'}).then((result) =>{
   //   console.log(result);
   // });
-
+  //
   // db.collection('Users').findOneAndDelete({
   //   _id: new ObjectID("58d4e677034a5127e8ffbe62")
   // }).then((result) =>{
